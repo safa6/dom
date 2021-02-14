a705e3d0383bb83a2ff8020851c725cf926dc353
safa6 <77966847+safa6@users.noreply.github.com>
2021-02-14 23:54:41 +0100

    Add files via upload

---
+++ shopcart.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    update_amounts();
+    $('.qty,.price').on('keyup keypress blur change',
+    function(e){
+    update_amounts();
+
+    });
+});
+function update_amounts(){
+    var sum = 0.0;
+    $('#myTable > tbody > tr').each(function (){
+        var qty = $(this).find('.qty').val();
+        var price =$(this).find('.price').val();
+        var amount = (qty*price);
+        sum+=amount;
+        $(this).find('.amount').text(''+amount);
+    });
+    $('.total').text(sum);
+}
+var incrementQty;
+var decrementQty;
+var plusBtn = $(".cart-qty-plus");
+var minusBtn = $(".cart-qty-minus");
+var incrementQty = plusBtn.click(function() {
+    var $n = $(this)
+    .parent(".button-container")
+    .find(".qty");
+    $n.val(Number($n.val())+1);
+    update_amounts();
+});
+var decrementQty=minusBtn.click(function(){
+    var $n =$(this);
+    parent(".button-container")
+    .find(".qty");
+    var qtyVal = Number($n.val())
+    if(qtyVal > 0){
+        $n.val(qtyVal-1);
+
+    }
+    update_amounts();
+});
+
+
+    
+   
